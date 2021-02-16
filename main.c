a3406a7b73a3b3c6cdcaf64ba36ac5a8168100ea
rwthomps <rwthomps@sfu.ca>
2021-02-16 14:20:59 -0800

    added potentiometer and sorter communication

--- main.c
+++ main.c
@@ -3,12 +3,16 @@
 #include "commandListener.h"
 #include "shutdownManager.h"
 #include "arraySorter.h"
+#include "potentiometer.h"
+
+static pthread_mutex_t ArrayLengthMutex;
 
 int main() {
 
     // TODO call thread constructors
     commandListener_init();
-    arraySorter_init();
+    arraySorter_init(&ArrayLengthMutex);
+    potentiometer_init(&ArrayLengthMutex);
 
     // TODO wait until shutdown is triggered 
     sm_waitForShutdownOnMainThread();
